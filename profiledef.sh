6bbb028082357c59a72a139adb4a79dc0f226d28
indra-zen <2450047@student.ppkia.ac.id~>
2024-12-09 00:16:40 +0800

    Add a new user

--- profiledef.sh
+++ profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="tagaroa"
-iso_label="TAGAROA_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_label="TAGAROA_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%m%Y)"
 iso_publisher="Tagaroa <https://github.com/Tagaroa>"
 iso_application="Tagaroa Live System"
-iso_version="0.0.1_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+iso_version="0.0.2-$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%d.%m.%Y)"
 install_dir="tagaroa"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
@@ -17,7 +17,8 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
-  ["/etc/shadow"]="0:0:400"
+  ["/etc/shadow"]="0:0:0400"
+  ["/etc/gshadow"]="0:0:0400"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
